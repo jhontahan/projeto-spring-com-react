9963f4061685709611222d974223c147b9cc3c33
jhonathan <jhonathancruz48@gmail.com>
2022-08-15 12:03:43 -0300

    criando endpoint de buscar por id

--- src/main/java/com/jhonathan/api/resource/LancamentoResource.java
+++ src/main/java/com/jhonathan/api/resource/LancamentoResource.java
@@ -93,6 +93,14 @@ public class LancamentoResource {
 		
 	}
 	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id)
+				.map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+		
+	}
+	
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
 		Lancamento lancamento = converter(dto);
@@ -162,6 +170,19 @@ public class LancamentoResource {
 			
 		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
 		
+	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+				.id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
 	}
 	
 	private Lancamento converter(LancamentoDTO dto) {
