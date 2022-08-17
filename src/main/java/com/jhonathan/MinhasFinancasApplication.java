3a4f92d2f16fa39249cc325ced9dde24d54ab411
jhonathan <jhonathancruz48@gmail.com>
2022-08-17 16:50:07 -0300

    classe de configuração

--- src/main/java/com/jhonathan/MinhasFinancasApplication.java
+++ src/main/java/com/jhonathan/MinhasFinancasApplication.java
@@ -2,19 +2,10 @@ package com.jhonathan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class MinhasFinancasApplication implements WebMvcConfigurer{
+public class MinhasFinancasApplication{
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasFinancasApplication.class, args);
 	}
